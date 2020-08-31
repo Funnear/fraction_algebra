2e40fabe3b658e2e2bc3fd6dd213f5b04b63a2a8
Fani P <funnear@gmail.com>
2020-08-31 19:44:21 +0300

    Removed redundant test cases from output tests

--- tests/output_test.py
+++ tests/output_test.py
@@ -6,12 +6,9 @@ from fraction.fraction import Fraction
 class TestOutput:
 
     @pytest.mark.parametrize("a, b, expected_output", [
-        (2, 3, "2/3"),
         (6, 9, "2/3"),
-        (3, 2, "1 1/2"),
         (15, 5, "3"),
         (15, 6, "2 1/2"),
-        (8, 7, "1 1/7"),
         (400, -200, "-2"),
         (2, -6, "-1/3"),
         (-25, 10, "-2 1/2"),
